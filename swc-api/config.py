1f545751e133dbab974b0acea61efa8502677dcb
aehaynes <a.emerson.haynes@gmail.com>
2015-05-26 01:54:12 -0400

    Update config.py

--- swc-api/config.py
+++ swc-api/config.py
@@ -6,12 +6,12 @@ USER_AGENT_STR = 'python-requests/1.1.0 CPython/2.7.3 Windows/7'
 SERVICE_URL = 'sealswithclubs.eu'#'https://sealswithclubs.eu'
 SERVICE_PORT = 8088
 MEANINGFUL_COLORS_URL = 'https://sealswithclubs.eu/meaningfulcolors.php'
-SWC_CERT_FILEPATH = 'sealswithclubs.eu.crt' #change this path
+SWC_CERT_FILEPATH = 'sealswithclubs.eu.crt'
 END_STR = '\x00'
 UNNAMED_HAND_IDENTIFIER = 'UNDEFINED'
 SITTINGOUT_STR = 'Sitting Out'
 
-BASE_DIR = '/home/nonroot/Projects/'
+BASE_DIR = '/home/nonroot/Projects/' #Change this path
 HH_PATH = BASE_DIR + 'swc-api/outdir/'
 DB_INFO = ['swc','postgres', 'postgres']
 
